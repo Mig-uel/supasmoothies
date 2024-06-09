dba46d9ecd0da7afa5fa859b3a99eec3c5b0b6d7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-09 11:33:14 -0400

    chore: Update supabase client configuration to use Vite environment variables

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.REACT_APP_SUBABASE_URL
-const supabaseKey = import.meta.env.REACT_APP_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_REACT_APP_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
