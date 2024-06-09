33dc284c470250c88e477d3b030c9b8c62126307
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-09 11:59:08 -0400

    chore: Refactor home page component to fetch and display smoothies

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from 'react'
 import supabase from '../config/supabaseClient'
 
+// components
+import SmoothieCard from '../components/smoothie-card.component'
+
 const HomePage = () => {
-  console.log(supabase)
+  const [smoothies, setSmoothies] = useState(null)
+  const [fetchError, setFetchError] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from('smoothies').select()
+
+      if (error) {
+        setFetchError(error.message)
+        setSmoothies(null)
+      }
+
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+  }, [])
 
   return (
     <div className='page home'>
-      <h2>Home</h2>
+      {smoothies ? (
+        <div className='smoothies'>
+          {smoothies.map((smoothie) => (
+            <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+          ))}
+        </div>
+      ) : (
+        <p>{fetchError}</p>
+      )}
     </div>
   )
 }
