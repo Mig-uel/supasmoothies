b95e39b27bd604a30532a52a1111cd6a5ec9499a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-10 15:47:57 -0400

    Refactor home page to lazy load SmoothieCard component and add react-router-dom Link

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState, lazy, Suspense } from 'react'
 import supabase from '../config/supabaseClient'
+import { Link } from 'react-router-dom'
 
 // components
 const SmoothieCard = lazy(() => import('../components/smoothie-card.component'))
 const ErrorCard = lazy(() => import('../components/error-card.component'))
 import { RingLoader } from 'react-spinners'
-import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   const [smoothies, setSmoothies] = useState(null)
