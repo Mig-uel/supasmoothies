a418c93a0e9eb7f030e31ca20499edad350686c2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-10 16:20:08 -0400

    refactor: Update UpdatePage component to include form submission handler and error handling

--- src/pages/update.page.jsx
+++ src/pages/update.page.jsx
@@ -1,14 +1,77 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import supabase from '../config/supabaseClient'
 
 const UpdatePage = () => {
   const { id } = useParams()
-  const [smoothie, setSmoothie] = useState(null)
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState(0)
+  const [formError, setFormError] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+        .eq('id', id)
+        .single()
+
+      if (error) {
+        navigate('/', { replace: true })
+      }
+
+      if (data) {
+        setTitle(data.title)
+        setMethod(data.method)
+        setRating(data.rating)
+      }
+    }
+
+    fetchSmoothie()
+  }, [id, navigate])
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    if (!title || !method || !rating) {
+      setFormError('Please fill out all fields!')
+      return
+    }
+  }
 
   return (
     <div className='page update'>
-      <h2>Update</h2>
+      <form onSubmit={submitHandler}>
+        <label htmlFor='title'>Title:</label>
+        <input
+          type='text'
+          id='title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor='title'>Method:</label>
+        <input
+          type='text'
+          id='method'
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor='rating'>Rating:</label>
+        <input
+          type='number'
+          id='rating'
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button type='submit'>Update Smoothie</button>
+        {formError && <p className='error'>{formError}</p>}
+      </form>
     </div>
   )
 }
