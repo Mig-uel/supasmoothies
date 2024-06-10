a75f643e93eff2a7c38cef71ce51403d228d02cf
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-10 15:44:31 -0400

    refactor: Add form validation to CreatePage component

--- src/pages/create.page.jsx
+++ src/pages/create.page.jsx
@@ -1,7 +1,74 @@
+import { useState } from 'react'
+import supabase from '../config/supabaseClient'
+import { useNavigate } from 'react-router-dom'
+
 const CreatePage = () => {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState(0)
+  const [formError, setFormError] = useState(null)
+
+  const navigate = useNavigate()
+
+  // submit handler
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    if (!title || !method || !rating) {
+      setFormError('Please fill out all fields!')
+      return
+    }
+
+    try {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .insert([{ title, method, rating }])
+        .select()
+
+      if (error) {
+        throw new Error('Please try again!')
+      }
+
+      setTitle('')
+      setMethod('')
+      setRating(0)
+      setFormError(null)
+      navigate('/')
+    } catch (error) {
+      setFormError(error.message)
+    }
+  }
+
   return (
     <div className='page create'>
-      <h2>Create</h2>
+      <form onSubmit={submitHandler}>
+        <label htmlFor='title'>Title:</label>
+        <input
+          type='text'
+          id='title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor='method'>Method:</label>
+        <input
+          type='text'
+          id='method'
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor='title'>Rating:</label>
+        <input
+          type='number'
+          id='rating'
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button type='submit'>Create Smoothie Recipe</button>
+        {formError && <p className='error'>{formError}</p>}
+      </form>
     </div>
   )
 }
