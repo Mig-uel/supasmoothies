24dfaf634a796f932429fe649b859234a86351bd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-10 15:50:34 -0400

    refactor: Add useParams hook and smoothie state to UpdatePage component

--- src/pages/update.page.jsx
+++ src/pages/update.page.jsx
@@ -1,4 +1,11 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import supabase from '../config/supabaseClient'
+
 const UpdatePage = () => {
+  const { id } = useParams()
+  const [smoothie, setSmoothie] = useState(null)
+
   return (
     <div className='page update'>
       <h2>Update</h2>
